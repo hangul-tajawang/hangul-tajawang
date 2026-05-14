fa543423cd068399ccb8781d847fd79359c8398d
iammoneytaker <dnjs2618@naver.com>
2026-05-14 21:54:36 +0900

    ADD

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/auth/', '/mypage/'], // 민감한 정보가 있는 페이지는 제외
+      disallow: ['/auth/', '/mypage/', '/*?_rsc='], // 민감한 정보 및 불필요한 RSC 크롤링 차단
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
